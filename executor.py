5e02c69bc8bb2d0917d0153818c1a76e05ef266a
xunwu <yangxunwu@gmail.com>
2026-03-27 00:20:04 +0800

    update

--- executor.py
+++ executor.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from py_clob_client.client import ClobClient, ClobAuth
+from py_clob_client.client import ClobClient
 from py_clob_client.clob_types import OrderArgs, OrderType
 from dotenv import load_dotenv
 
